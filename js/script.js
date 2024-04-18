1f441f98d9909270b6edcb1187dfeb46db0be6c0
lucianocretella95 <luciano.cretella@tim.it>
2024-04-18 17:57:47 +0200

    form

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
-// let km = prompt ("chilometri da percorrere");
-// console.log(km);
-// let age = prompt ("età passeggero");
-// console.log(age);
 const biglietto = 0.21;
-let km = "chilometri";
-let age = "eta";
-let prezzo = km * 0.21;
+let km = document.getElementById("kmarea");
+console.log(km);
+let age = document.getElementById("agearea");
+console.log(age);
+let prezzo = kmarea * 0.21;
+console.log(prezzo);
 const pricebutton = document.getElementById("price-button");
-const ageinput = document.getElementById("age-input");
-const kilometer = document.getElementById("km-input");
+console.log(pricebutton);
 let sconto20 = (prezzo * 20) / 100;
+console.log(sconto20);
 let sconto40 = (prezzo * 40) / 100;
-if (age < 18) {
-  prezzo = prezzo - sconto20;
-} else if (age >= 65) {
-  prezzo = prezzo - sconto40;
-}
+console.log(sconto40);
 pricebutton.addEventListener("click", function () {
+  if (age < 18) {
+    prezzo = prezzo - sconto20;
+  } else if (age >= 65) {
+    prezzo = prezzo - sconto40;
+  }
   document.getElementById("price-button").innerHTML = ` ${prezzo}`;
 });
