2f79cdb365b71003c342dd6136015fd2c3b5c84d
lucianocretella95 <luciano.cretella@tim.it>
2024-04-18 17:03:45 +0200

    commit form

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
+// let km = prompt ("chilometri da percorrere");
+// console.log(km);
+// let age = prompt ("età passeggero");
+// console.log(age);
 const biglietto = 0.21;
-let km = prompt ("chilometri da percorrere");
-console.log(km);
-let age = prompt ("età passeggero");
-console.log(age);
+let km = "chilometri";
+let age = "eta";
 let prezzo = km * 0.21;
+const pricebutton = document.getElementById("price-button");
+const ageinput = document.getElementById("age-input");
+const kilometer = document.getElementById("km-input");
 let sconto20 = (prezzo * 20) / 100;
 let sconto40 = (prezzo * 40) / 100;
 if (age < 18) {
-    prezzo = prezzo - sconto20;
-  } else if (age >= 65) {
-    prezzo = prezzo - sconto40;
-  // } else {
-  //   alert("nessuno sconto");
-  }
-document.getElementById("standard").innerText= `il tuo biglietto costa: ${prezzo}`;
+  prezzo = prezzo - sconto20;
+} else if (age >= 65) {
+  prezzo = prezzo - sconto40;
+}
+pricebutton.addEventListener("click", function () {
+  document.getElementById("price-button").innerHTML = ` ${prezzo}`;
+});
